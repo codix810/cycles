5f40703699e00ebeb96f05059c7e3583a090da83
CodiX 🧑‍💻 <codix810@gmail.com>
2026-02-09 23:59:04 +0200

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
+
